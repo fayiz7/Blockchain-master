1f69c251b6121bb3497a91ecf577720f83cb725f
Fayiz7 <fayiz7.kau@gmail.com>
2021-02-01 22:17:04 +0545

    Initial commit

---
+++ src/main/java/agent/Testing.java
@@ -0,0 +1,27 @@
+package agent;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Testing {
+
+    public static void main(String[] args) {
+      //  List<Block> blockchain2 = new ArrayList<>();
+
+        // add elements in the arraylist
+//        animals.add("Cat");
+//        animals.add("Dog");
+//        animals.add("Cow");
+//        System.out.println("ArrayList: " + animals);
+
+        // get the element from the arraylist
+//        String str = animals.get(1);
+//        System.out.print("Element at index 1: " + str);
+
+    }
+    public static  void print(List<Block> b){
+        System.out.println("Ya Fayiz this is the latest update:"+b.toString());
+
+    }
+
+
+}
