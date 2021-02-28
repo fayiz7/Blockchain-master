d5044e2a6587674792f2e5b01ee205d2c308aa67
Fayiz7 <fayiz7.kau@gmail.com>
2021-03-01 01:40:04 +0545

    update

---
+++ src/main/java/agent/Main7.java
@@ -0,0 +1,15 @@
+public class Main7{
+
+
+
+
+public static void main(String[] args){
+    System.out.println("teset");
+    Node node = new Node(8);
+    node.getChildren();
+    node.getData();
+    node.addChild(new Node(7));
+    System.out.println(node.getChildren());
+}
+
+}
